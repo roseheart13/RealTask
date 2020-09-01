9cd38f78ccdd4e5f765172c54bca54bf63930df8
Rutha <70570030+roseheart13@users.noreply.github.com>
2020-09-01 15:54:33 +0200

    Finish

--- RealTask/RealTask.cpp
+++ RealTask/RealTask.cpp
@@ -1,48 +1,48 @@
 
 #include <iostream>
 #include <string>
+//
+///*
+//    int  age = 0;
+//    std::string name = "";
+//
+//    int main() {
+//        std::cout << "Pleas enter your age :";
+//        std::cin >> age;
+//        std::cout << "What is your full naME :";
+//        std::cin.ignore();
+//        std::getline(std::cin, name);
+//
+//        if (age >= 20) {
+//            std::cout << "You are an adult";
+//        }
+//        if (age < 20) {
+//            std::cout << "You are a teenager";
+//        }
+//
+//        std::cout << "Your name is :" << name << " and you are " << age << " years old" << std::endl
+//
+//        return 0;
+//    }
+//    */
 
 
-    int  age = 0;
-    std::string name = "";
-
-    int main() {
-        std::cout << "Pleas enter your age :";
-        std::cin >> age;
-        std::cout << "What is your full naME :";
-        std::cin.ignore();
-        std::getline(std::cin, name);
-
-        if (age >= 20) {
-            std::cout << "You are an adult";
-        }
-        if (age < 20) {
-            std::cout << "You are a teenager";
-        }
-
-        std::cout << "Your name is :" << name << " and you are " << age << " years old" << std::endl
-
-        return 0;
-    }
+    int age;
 
 
-
-/*int age;
-
-
-int main()
+    int main()
 {
-    std::cout << "What is your age: ";
-    std::cin >> age;
-    std::cout << "Your age is " << age << std::endl;
+        std::cout << "What is your age: ";
+        std::cin >> age;
+        std::cout << "Your age is " << age << std::endl;
 
-    if (age >= 20) {
+            if (age >= 20) {
 
-        std::cout << "You are an adult";
+                 std::cout << "You are an adult";
     }
 
-    if (age < 20); {
+            if (age < 20) {
 
-        std::cout << "You are a teenager";
-   } */
+                std::cout << "You are a teenager";
+   } 
 }

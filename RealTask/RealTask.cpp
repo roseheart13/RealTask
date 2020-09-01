dbf779d038b24d5ac68dc2b94ba645d3018e5e51
Rutha <70570030+roseheart13@users.noreply.github.com>
2020-09-01 15:30:46 +0200

    hmmmmmm

--- RealTask/RealTask.cpp
+++ RealTask/RealTask.cpp
@@ -2,13 +2,32 @@
 #include <iostream>
 #include <string>
 
-/*
+
     int  age = 0;
+    std::string name = "";
+
+    int main() {
+        std::cout << "Pleas enter your age :";
+        std::cin >> age;
+        std::cout << "What is your full naME :";
+        std::cin.ignore();
+        std::getline(std::cin, name);
+
+        if (age >= 20) {
+            std::cout << "You are an adult";
+        }
+        if (age < 20) {
+            std::cout << "You are a teenager";
+        }
+
+        std::cout << "Your name is :" << name << " and you are " << age << " years old" << std::endl
+
+        return 0;
+    }
 
 
-*/
 
-int age;
+/*int age;
 
 
 int main()
@@ -22,8 +41,8 @@ int main()
         std::cout << "You are an adult";
     }
 
-    else (age < 20); {
+    if (age < 20); {
 
         std::cout << "You are a teenager";
-   }
+   } */
 }
